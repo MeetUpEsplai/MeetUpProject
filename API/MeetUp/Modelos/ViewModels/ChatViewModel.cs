351cb225e05443a4d14a04a84ec3da9c20e2e165
Roger <roger-d-n@hotmail.com>
2023-05-03 13:05:26 +0200

    Creación viewmodels

---
+++ API/MeetUp/Modelos/ViewModels/ChatViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetUp.Modelos.ViewModels
+{
+    public class ChatViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
---
+++ API/MeetUp/Modelos/ViewModels/TipoReaccionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetUp.Modelos.ViewModels
+{
+    public class TipoReaccionViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Emoji { get; set; }
+    }
+}
