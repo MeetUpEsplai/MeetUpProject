3ca4abaf52c8f1e4b8809d9ac95e8416507c22c7
DKNathan7 <105308225+DKNathan7@users.noreply.github.com>
2023-05-11 02:06:01 +0200

    Add files via upload

---
+++ evento/js/reacciones.js
@@ -0,0 +1,24 @@
+function gusta(){
+    document.getElementById('tituloReaccion').innerHTML='Me Gusta';
+}
+
+function encanta(){
+    document.getElementById('tituloReaccion').innerHTML='Me Encanta';
+}
+
+function divierte(){
+    document.getElementById('tituloReaccion').innerHTML='Me Divierte';
+}
+
+function asombra(){
+    document.getElementById('tituloReaccion').innerHTML='Me Asombra';
+}
+
+function entristece(){
+    document.getElementById('tituloReaccion').innerHTML='Me Entristece';
+}
+
+function enfada(){
+    document.getElementById('tituloReaccion').innerHTML='Me Enfada';
+}
+
